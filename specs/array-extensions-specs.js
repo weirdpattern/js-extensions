4b33a8ec718000430647407c4a1415195e8d806d
weirdpattern <patricio@weirdpattern.com>
2017-03-07 23:31:25 -0600

    Add comments to array extensions Complete flatten array extension tests Add new number extensions Add number extension tests

--- specs/array-extensions-specs.js
+++ specs/array-extensions-specs.js
@@ -1,7 +1,7 @@
-import flatten from '../lib/array-extensions';
+import { flatten } from '../lib/array-extensions';
 
 describe('flatten', () => {
-  it('returns an array when a non array is passed', () => {
+  it('returns a single array when non-array values are passed', () => {
     expect(flatten(true)).toEqual([true]);
     expect(flatten(true, false)).toEqual([true, false]);
     expect(flatten(0)).toEqual([0]);
@@ -10,25 +10,25 @@ describe('flatten', () => {
     expect(flatten('', 'a')).toEqual(['', 'a']);
   });
 
-  it('returns an array when a single array is passed', () => {
+  it('returns the same array when a single array is passed', () => {
     expect(flatten([true, false, false])).toEqual([true, false, false]);
     expect(flatten([1, 2, 3])).toEqual([1, 2, 3]);
     expect(flatten(['', 'a', 'b'])).toEqual(['', 'a', 'b']);
   });
 
-  it('returns an array when an array and non-arrays are passed', () => {
+  it('returns a flattened array when an array and non-arrays are passed', () => {
     expect(flatten([true, false, false], true, false)).toEqual([true, false, false, true, false]);
     expect(flatten([1, 2, 3], 4, 5)).toEqual([1, 2, 3, 4, 5]);
     expect(flatten(['', 'a', 'b'], 'c', 'd')).toEqual(['', 'a', 'b', 'c', 'd']);
   });
 
-  it('returns an array when multiple arrays are passed', () => {
+  it('returns a flattened array when multiple arrays are passed', () => {
     expect(flatten([true, false, false], [true, false])).toEqual([true, false, false, true, false]);
     expect(flatten([1, 2, 3], [4, 5])).toEqual([1, 2, 3, 4, 5]);
     expect(flatten(['', 'a', 'b'], ['c', 'd'])).toEqual(['', 'a', 'b', 'c', 'd']);
   });
 
-  it('returns an array when multiple arrays with sub-arrays are passed', () => {
+  it('returns a flattened array when multiple arrays with sub-arrays are passed', () => {
     expect(
       flatten([true, [[false], false]], [[true], false])
     ).toEqual([true, false, false, true, false]);
@@ -38,7 +38,7 @@ describe('flatten', () => {
 });
 
 describe('Array.flatten', () => {
-  it('returns an array when a non array is passed', () => {
+  it('returns a single array when non-array values are passed', () => {
     expect(Array.flatten(true)).toEqual([true]);
     expect(Array.flatten(true, false)).toEqual([true, false]);
     expect(Array.flatten(0)).toEqual([0]);
@@ -47,13 +47,13 @@ describe('Array.flatten', () => {
     expect(Array.flatten('', 'a')).toEqual(['', 'a']);
   });
 
-  it('returns an array when a single array is passed', () => {
+  it('returns the same array when a single array is passed', () => {
     expect(Array.flatten([true, false, false])).toEqual([true, false, false]);
     expect(Array.flatten([1, 2, 3])).toEqual([1, 2, 3]);
     expect(Array.flatten(['', 'a', 'b'])).toEqual(['', 'a', 'b']);
   });
 
-  it('returns an array when an array and non-arrays are passed', () => {
+  it('returns a flattened array when an array and non-arrays are passed', () => {
     expect(
       Array.flatten([true, false, false], true, false)
     ).toEqual([true, false, false, true, false]);
@@ -61,7 +61,7 @@ describe('Array.flatten', () => {
     expect(Array.flatten(['', 'a', 'b'], 'c', 'd')).toEqual(['', 'a', 'b', 'c', 'd']);
   });
 
-  it('returns an array when multiple arrays are passed', () => {
+  it('returns a flattened array when multiple arrays are passed', () => {
     expect(
       Array.flatten([true, false, false], [true, false])
     ).toEqual([true, false, false, true, false]);
@@ -69,7 +69,7 @@ describe('Array.flatten', () => {
     expect(Array.flatten(['', 'a', 'b'], ['c', 'd'])).toEqual(['', 'a', 'b', 'c', 'd']);
   });
 
-  it('returns an array when multiple arrays with sub-arrays are passed', () => {
+  it('returns a flattened array when multiple arrays with sub-arrays are passed', () => {
     expect(
       Array.flatten([true, [[false], false]], [[true], false])
     ).toEqual([true, false, false, true, false]);
@@ -79,12 +79,57 @@ describe('Array.flatten', () => {
 });
 
 describe('[].flatten', () => {
-  it('returns an array when a non array is passed', () => {
+  it('flattens the current array when no values are passed', () => {
+    expect([true].flatten()).toEqual([true]);
+    expect([true, false].flatten()).toEqual([true, false]);
+    expect([true, [false]].flatten()).toEqual([true, false]);
+    expect([true, [false, [true]]].flatten()).toEqual([true, false, true]);
+    expect([0].flatten()).toEqual([0]);
+    expect([0, 1].flatten()).toEqual([0, 1]);
+    expect([0, [1]].flatten()).toEqual([0, 1]);
+    expect([0, [1, [2]]].flatten()).toEqual([0, 1, 2]);
+    expect([''].flatten()).toEqual(['']);
+    expect(['', 'a'].flatten()).toEqual(['', 'a']);
+    expect(['', ['a']].flatten()).toEqual(['', 'a']);
+    expect(['', ['a', ['b']]].flatten()).toEqual(['', 'a', 'b']);
+  });
+
+  it('returns a single array when non-array values are passed', () => {
     expect([true].flatten(true)).toEqual([true, true]);
     expect([true, false].flatten(true, false)).toEqual([true, false, true, false]);
     expect([0].flatten(0)).toEqual([0, 0]);
     expect([0, 1].flatten(0, 1)).toEqual([0, 1, 0, 1]);
     expect([''].flatten('')).toEqual(['', '']);
     expect(['', 'a'].flatten('', 'a')).toEqual(['', 'a', '', 'a']);
+  });
+
+  it('returns the same array when a single array is passed', () => {
+    expect([true].flatten([true, false, false])).toEqual([true, true, false, false]);
+    expect([1].flatten([1, 2, 3])).toEqual([1, 1, 2, 3]);
+    expect([''].flatten(['', 'a', 'b'])).toEqual(['', '', 'a', 'b']);
+  });
+
+  it('returns a flattened array when an array and non-arrays are passed', () => {
+    expect(
+      [true].flatten([true, false, false], true, false)
+    ).toEqual([true, true, false, false, true, false]);
+    expect([1].flatten([1, 2, 3], 4, 5)).toEqual([1, 1, 2, 3, 4, 5]);
+    expect([''].flatten(['', 'a', 'b'], 'c', 'd')).toEqual(['', '', 'a', 'b', 'c', 'd']);
+  });
+
+  it('returns a flattened array when multiple arrays are passed', () => {
+    expect(
+      [true].flatten([true, false, false], [true, false])
+    ).toEqual([true, true, false, false, true, false]);
+    expect([1].flatten([1, 2, 3], [4, 5])).toEqual([1, 1, 2, 3, 4, 5]);
+    expect([''].flatten(['', 'a', 'b'], ['c', 'd'])).toEqual(['', '', 'a', 'b', 'c', 'd']);
+  });
+
+  it('returns a flattened array when multiple arrays with sub-arrays are passed', () => {
+    expect(
+      [true].flatten([true, [[false], false]], [[true], false])
+    ).toEqual([true, true, false, false, true, false]);
+    expect([1].flatten([1, [[2], 3]], [4, 5])).toEqual([1, 1, 2, 3, 4, 5]);
+    expect([''].flatten(['', [['a'], 'b']], ['c', 'd'])).toEqual(['', '', 'a', 'b', 'c', 'd']);
   });
 });
