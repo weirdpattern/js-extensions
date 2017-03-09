35394e30b0d4fa363dd843c85b83c2627dbb53b7
weirdpattern <patricio@weirdpattern.com>
2017-03-08 23:14:02 -0600

    Fix array-extensions first method assertion Add new clear, first and head method tests Remove .idea files

--- specs/array-extensions-specs.js
+++ specs/array-extensions-specs.js
@@ -1,4 +1,248 @@
-import { flatten } from '../lib/array-extensions';
+import {
+  clear,
+  first,
+  flatten,
+  head
+} from '../lib/array-extensions';
+
+describe('clear', () => {
+  it('clears the content of an array', () => {
+    const array = [1, 2, 3, 4];
+    expect(clear(array)).toEqual([1, 2, 3, 4]);
+    expect(array).toEqual([]);
+  });
+
+  it('throws exception when a non-array is passed', () => {
+    expect(() => { clear(true); }).toThrow();
+    expect(() => { clear(0); }).toThrow();
+    expect(() => { clear(''); }).toThrow();
+    expect(() => { clear({}); }).toThrow();
+  });
+});
+
+describe('Array.clear', () => {
+  it('clears the content of an array', () => {
+    const array = [1, 2, 3, 4];
+    expect(Array.clear(array)).toEqual([1, 2, 3, 4]);
+    expect(array).toEqual([]);
+  });
+
+  it('throws exception when a non-array is passed', () => {
+    expect(() => { Array.clear(true); }).toThrow();
+    expect(() => { Array.clear(0); }).toThrow();
+    expect(() => { Array.clear(''); }).toThrow();
+    expect(() => { Array.clear({}); }).toThrow();
+  });
+});
+
+describe('[].clear', () => {
+  it('clears the content of an array', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.clear()).toEqual([1, 2, 3, 4]);
+    expect(array).toEqual([]);
+  });
+});
+
+describe('first', () => {
+  it('returns the first element of an array', () => {
+    expect(first([1, 2, 3, 4])).toBe(1);
+    expect(first([1, 2, 3, 4], 1)).toBe(1);
+  });
+
+  it('returns the first n elements of an array', () => {
+    expect(first([1, 2, 3, 4], 2)).toEqual([1, 2]);
+    expect(first([1, 2, 3, 4], 3)).toEqual([1, 2, 3]);
+    expect(first([1, 2, 3, 4], 4)).toEqual([1, 2, 3, 4]);
+  });
+
+  it('returns all elements of an array when the count exceeds the length', () => {
+    expect(first([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    expect(first([1, 2, 3, 4], 6)).toEqual([1, 2, 3, 4]);
+    expect(first([1, 2, 3, 4], 7)).toEqual([1, 2, 3, 4]);
+  });
+
+  it('does not modify the original array', () => {
+    const array = [1, 2, 3, 4];
+    expect(first(array, 2)).toEqual([1, 2]);
+    expect(array).toEqual([1, 2, 3, 4]);
+  });
+
+  it('throws an exception when a non-array is passed', () => {
+    expect(() => { first(true); }).toThrow();
+    expect(() => { first(0); }).toThrow();
+    expect(() => { first(''); }).toThrow();
+    expect(() => { first({}); }).toThrow();
+
+    expect(() => { first(true, 1); }).toThrow();
+    expect(() => { first(0, 1); }).toThrow();
+    expect(() => { first('', 1); }).toThrow();
+    expect(() => { first({}, 1); }).toThrow();
+  });
+
+  it('throws an exception when the count is less than 1', () => {
+    expect(() => { first([1, 2, 3, 4], 0); }).toThrow();
+    expect(() => { first([1, 2, 3, 4], -1); }).toThrow();
+    expect(() => { first([1, 2, 3, 4], -2); }).toThrow();
+  });
+});
+
+describe('Array.first', () => {
+  it('returns the first element of an array', () => {
+    expect(Array.first([1, 2, 3, 4])).toBe(1);
+    expect(Array.first([1, 2, 3, 4], 1)).toBe(1);
+  });
+
+  it('returns the first n elements of an array', () => {
+    expect(Array.first([1, 2, 3, 4], 2)).toEqual([1, 2]);
+    expect(Array.first([1, 2, 3, 4], 3)).toEqual([1, 2, 3]);
+    expect(Array.first([1, 2, 3, 4], 4)).toEqual([1, 2, 3, 4]);
+  });
+
+  it('returns all elements of an array when the count exceeds the length', () => {
+    expect(Array.first([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    expect(Array.first([1, 2, 3, 4], 6)).toEqual([1, 2, 3, 4]);
+    expect(Array.first([1, 2, 3, 4], 7)).toEqual([1, 2, 3, 4]);
+  });
+
+  it('does not modify the original array', () => {
+    const array = [1, 2, 3, 4];
+    expect(Array.first(array, 2)).toEqual([1, 2]);
+    expect(array).toEqual([1, 2, 3, 4]);
+  });
+
+  it('throws an exception when a non-array is passed', () => {
+    expect(() => { Array.first(true); }).toThrow();
+    expect(() => { Array.first(0); }).toThrow();
+    expect(() => { Array.first(''); }).toThrow();
+    expect(() => { Array.first({}); }).toThrow();
+
+    expect(() => { Array.first(true, 1); }).toThrow();
+    expect(() => { Array.first(0, 1); }).toThrow();
+    expect(() => { Array.first('', 1); }).toThrow();
+    expect(() => { Array.first({}, 1); }).toThrow();
+  });
+
+  it('throws an exception when the count is less than 1', () => {
+    expect(() => { Array.first([1, 2, 3, 4], 0); }).toThrow();
+    expect(() => { Array.first([1, 2, 3, 4], -1); }).toThrow();
+    expect(() => { Array.first([1, 2, 3, 4], -2); }).toThrow();
+  });
+});
+
+describe('[].first', () => {
+  it('returns the first element of an array', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.first()).toBe(1);
+    expect(array.first(1)).toBe(1);
+  });
+
+  it('returns the first n elements of an array', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.first(2)).toEqual([1, 2]);
+    expect(array.first(3)).toEqual([1, 2, 3]);
+    expect(array.first(4)).toEqual([1, 2, 3, 4]);
+  });
+
+  it('returns all elements of an array when the count exceeds the length', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.first(5)).toEqual([1, 2, 3, 4]);
+    expect(array.first(6)).toEqual([1, 2, 3, 4]);
+    expect(array.first(7)).toEqual([1, 2, 3, 4]);
+  });
+
+  it('does not modify the original array', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.first(2)).toEqual([1, 2]);
+    expect(array).toEqual([1, 2, 3, 4]);
+  });
+
+  it('throws an exception when the count is less than 1', () => {
+    const array = [1, 2, 3, 4];
+    expect(() => { array.first(0); }).toThrow();
+    expect(() => { array.first(-1); }).toThrow();
+    expect(() => { array.first(-2); }).toThrow();
+  });
+});
+
+describe('head', () => {
+  it('returns the first element of an array', () => {
+    expect(head([1, 2, 3, 4])).toBe(1);
+  });
+
+  it('ignores other parameters passed', () => {
+    expect(head([1, 2, 3, 4], 2)).toEqual(1);
+    expect(head([1, 2, 3, 4], 3)).toEqual(1);
+    expect(head([1, 2, 3, 4], 4)).toEqual(1);
+  });
+
+  it('does not modify the original array', () => {
+    const array = [1, 2, 3, 4];
+    expect(head(array)).toEqual(1);
+    expect(array).toEqual([1, 2, 3, 4]);
+  });
+
+  it('throws an exception when a non-array is passed', () => {
+    expect(() => { head(true); }).toThrow();
+    expect(() => { head(0); }).toThrow();
+    expect(() => { head(''); }).toThrow();
+    expect(() => { head({}); }).toThrow();
+
+    expect(() => { head(true, 1); }).toThrow();
+    expect(() => { head(0, 1); }).toThrow();
+    expect(() => { head('', 1); }).toThrow();
+    expect(() => { head({}, 1); }).toThrow();
+  });
+});
+
+describe('Array.head', () => {
+  it('returns the first element of an array', () => {
+    expect(Array.head([1, 2, 3, 4])).toBe(1);
+  });
+
+  it('ignores other parameters passed', () => {
+    expect(Array.head([1, 2, 3, 4], 2)).toEqual(1);
+    expect(Array.head([1, 2, 3, 4], 3)).toEqual(1);
+    expect(Array.head([1, 2, 3, 4], 4)).toEqual(1);
+  });
+
+  it('does not modify the original array', () => {
+    const array = [1, 2, 3, 4];
+    expect(Array.head(array)).toEqual(1);
+    expect(array).toEqual([1, 2, 3, 4]);
+  });
+
+  it('throws an exception when a non-array is passed', () => {
+    expect(() => { Array.head(true); }).toThrow();
+    expect(() => { Array.head(0); }).toThrow();
+    expect(() => { Array.head(''); }).toThrow();
+    expect(() => { Array.head({}); }).toThrow();
+
+    expect(() => { Array.head(true, 1); }).toThrow();
+    expect(() => { Array.head(0, 1); }).toThrow();
+    expect(() => { Array.head('', 1); }).toThrow();
+    expect(() => { Array.head({}, 1); }).toThrow();
+  });
+});
+
+describe('[].head', () => {
+  it('returns the first element of an array', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.head()).toBe(1);
+  });
+
+  it('ignores other parameters passed', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.head(2)).toEqual(1);
+    expect(array.head(3)).toEqual(1);
+    expect(array.head(4)).toEqual(1);
+  });
+
+  it('does not modify the original array', () => {
+    const array = [1, 2, 3, 4];
+    expect(array.head()).toEqual(1);
+    expect(array).toEqual([1, 2, 3, 4]);
+  });
+});
 
 describe('flatten', () => {
   it('returns a single array when non-array values are passed', () => {
